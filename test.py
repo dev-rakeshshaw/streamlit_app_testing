e9fefebaeec745eb9093af492a931477b4cb7b4b
Rakesh <rakesh.shaw018@gmail.com>
2023-01-15 00:07:08 +0530

    Streamlit Testing

---
+++ test.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+from PIL import Image
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    image = Image.open(uploaded_file)
+    st.image(image, caption='HEllo Paji')
+
+    # # To convert to a string based IO:
+    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # st.write(stringio)
+
+    # # To read file as string:
+    # string_data = stringio.read()
+    # st.write(string_data)
+
+    # # Can be used wherever a "file-like" object is accepted:
+    # dataframe = pd.read_csv(uploaded_file)
+    # st.write(dataframe)
